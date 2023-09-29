13a145216c57fc5f3faffa1cbec250e24e20ace2
Dante-Berth <bertinalexAI@protonmail.com>
2023-09-29 23:13:04 +0200

    [UPDATE] tsmixer_res_block can be saved and used

--- model/tsmixer.py
+++ model/tsmixer.py
@@ -5,9 +5,9 @@ class tsmixer_cnn_dense_model(tf.keras.layers.Layer):
   """CNN model."""
 
   def __init__(self, n_channel, pred_len, kernel_size,*args,**kwargs):
-    super(tsmixer_cnn_dense_model).__init__(*args,**kwargs)
+    super().__init__(*args,**kwargs)
     self.cnn = tf.keras.layers.Conv1D(
-        n_channel, kernel_size, padding='same', input_shape=(None, n_channel)
+        n_channel, kernel_size, padding='same'
     )
     self.dense = tf.keras.layers.Dense(pred_len)
 
@@ -90,6 +90,7 @@ class RevNorm(tf.keras.layers.Layer):
     return x
 
 def res_block(inputs, norm_type, activation, dropout, ff_dim):
+  # Mixer Layer
   """Residual block of TSMixer."""
 
   norm = (
@@ -101,7 +102,7 @@ def res_block(inputs, norm_type, activation, dropout, ff_dim):
   # Temporal Linear
   x = norm(axis=[-2, -1])(inputs)
   x = tf.transpose(x, perm=[0, 2, 1])  # [Batch, Channel, Input Length]
-  x =  tf.keras.layers.Dense(x.shape[-1], activation=activation)(x)
+  x = tf.keras.layers.Dense(x.shape[-1], activation=activation)(x)
   x = tf.transpose(x, perm=[0, 2, 1])  # [Batch, Input Length, Channel]
   x = tf.keras.layers.Dropout(dropout)(x)
   res = x + inputs
@@ -115,6 +116,54 @@ def res_block(inputs, norm_type, activation, dropout, ff_dim):
   x = tf.keras.layers.Dense(inputs.shape[-1])(x)  # [Batch, Input Length, Channel]
   x = tf.keras.layers.Dropout(dropout)(x)
   return x + res
+
+@tf.keras.utils.register_keras_serializable(name="tsmixer_res_block")
+class tsmixer_res_block(tf.keras.layers.Layer):
+  def __init__(self,norm_type, activation, dropout, ff_dim,**kwargs):
+    super(tsmixer_res_block,self).__init__(**kwargs)
+    self.norm_type = norm_type
+    self.activation = activation
+    self.dropout = dropout
+    self.ff_dim = ff_dim
+    self.norm = (
+            tf.keras.layers.LayerNormalization
+            if norm_type == 'L'
+            else tf.keras.layers.BatchNormalization
+        )
+    self.dense_temporal_projection = None
+    self.dropout = tf.keras.layers.Dropout(dropout)
+    self.dense_time_mixing = None
+    self.dense_feature_mixing = None
+
+  def get_config(self):
+    config = super(tsmixer_res_block, self).get_config()
+    config.update({
+      'norm_type': self.norm_type,
+      'activation': self.activation,
+      'dropout': self.dropout,
+      'ff_dim': self.ff_dim
+    })
+    return config
+  def build(self,input_shape,*args,**kwargs):
+    self.dense_temporal_projection = tf.keras.layers.Dense(input_shape[-2], activation=self.activation)
+    self.dense_feature_mixing = tf.keras.layers.Dense(input_shape[-1], activation=self.activation)
+    self.dense_time_mixing = tf.keras.layers.Dense(self.ff_dim, activation=self.activation)
+  def call(self, inputs,*args,**kwargs):
+
+    x = self.norm(axis=[-2, -1])(inputs)
+    x = tf.transpose(x, perm=[0, 2, 1])  # [Batch, Channel, Input Length]
+    x = self.dense_temporal_projection(x)
+    x = tf.transpose(x, perm=[0, 2, 1])  # [Batch, Input Length, Channel]
+    x = self.dropout(x)
+    res = x + inputs
+
+    # Feature Linear
+    x = self.norm(axis=[-2, -1])(res)
+    x = self.dense_time_mixing(x)  # [Batch, Input Length, FF_Dim]
+    x = self.dropout(x)
+    x = self.dense_feature_mixing(x)  # [Batch, Input Length, Channel]
+    x = self.dropout(x)
+    return x + res
 
 def build_model(
       input_shape,
@@ -141,3 +190,25 @@ def build_model(
   outputs = tf.transpose(x, perm=[0, 2, 1])  # [Batch, Output Length, Channel])
 
   return tf.keras.Model(inputs, outputs)
+if __name__ == "__main__":
+  x = tf.ones((12,24,36))
+  inputs =  tf.keras.layers.Input(shape=(24, 36))
+  tsmixer = tsmixer_res_block(norm_type="L", activation="gelu", dropout=0.3, ff_dim=15)
+  print(tsmixer(x).shape)
+  y = tsmixer(inputs)
+
+  model = tf.keras.models.Model(inputs=inputs, outputs=y)
+  model.compile(
+    optimizer="Adam",
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+    metrics=["accuracy"]
+  )
+
+  print("Model before the loading")
+  model.summary()
+
+  PATH = 'testing_model_custom_activation_layer.h5'
+  model.save(PATH)
+  model = tf.keras.models.load_model(PATH)
+  print("Model loaded")
+  print(model.summary())
