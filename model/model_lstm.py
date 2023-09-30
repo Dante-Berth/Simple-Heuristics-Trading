[FIX] tsmixer.py with normalization and [ADD] tsmixer residual block model_lstm.py

model/model_lstm.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from simple_heuritic_tradings.model.tsmixer import tsmixer_res_block
 @tf.keras.utils.register_keras_serializable()
 class Signlog(tf.keras.layers.Layer):
     """
@@ -94,6 +95,16 @@ class super_lstm(tf.keras.layers.Layer):
         self.cnn_2_2 = tf.keras.layers.Conv1D(filters=32, kernel_size=4, strides=2)
         self.cnn_3_2 = tf.keras.layers.Conv1D(filters=8, kernel_size=2, strides=1)
 
+        self.tsmixer_res_block_1 = tsmixer_res_block(activation="gelu", dropout=0.3, ff_dim=16)
+        self.tsmixer_res_block_2 = tsmixer_res_block(activation="gelu", dropout=0.3, ff_dim=8)
+        self.tsmixer_res_block_3 = tsmixer_res_block(activation="gelu", dropout=0.3, ff_dim=4)
+
+        self.cnn_1_3 = tf.keras.layers.Conv1D(filters=64, kernel_size=4, strides=2)
+        self.cnn_2_3 = tf.keras.layers.Conv1D(filters=32, kernel_size=4, strides=2)
+        self.cnn_3_3 = tf.keras.layers.Conv1D(filters=8, kernel_size=2, strides=1)
+
+        self.mlp_10 = tf.keras.layers.Dense(1)
+
     def call(self,input):
         x_1 = self.lstm_1(input)
         x_1 = self.lstm_2(x_1)
@@ -136,6 +147,15 @@ class super_lstm(tf.keras.layers.Layer):
         x_transformer = tf.keras.layers.Flatten()(x_transformer)
         x_transformer = self.mlp_9(x_transformer)
 
-        x_5 = x_4 + y_2 + x_transformer
+        x_tsmixer = self.tsmixer_res_block_1(input)
+        x_tsmixer = self.tsmixer_res_block_2(x_tsmixer)
+        x_tsmixer = self.tsmixer_res_block_3(x_tsmixer)
+        x_tsmixer = self.cnn_1_3(x_tsmixer)
+        x_tsmixer = self.cnn_2_3(x_tsmixer)
+        x_tsmixer = self.cnn_3_3(x_tsmixer)
+        x_tsmixer = tf.keras.layers.Flatten()(x_tsmixer)
+        x_tsmixer = self.mlp_10(x_tsmixer)
+
+        x_5 = x_4 + y_2 + x_transformer + x_tsmixer
         return x_5
 

model/tsmixer.py
@@ -119,17 +119,12 @@ def res_block(inputs, norm_type, activation, dropout, ff_dim):
 
 @tf.keras.utils.register_keras_serializable(name="tsmixer_res_block")
 class tsmixer_res_block(tf.keras.layers.Layer):
-  def __init__(self,norm_type, activation, dropout, ff_dim,**kwargs):
+  def __init__(self, activation, dropout, ff_dim,**kwargs):
     super(tsmixer_res_block,self).__init__(**kwargs)
-    self.norm_type = norm_type
     self.activation = activation
     self.dropout = dropout
     self.ff_dim = ff_dim
-    self.norm = (
-            tf.keras.layers.LayerNormalization
-            if norm_type == 'L'
-            else tf.keras.layers.BatchNormalization
-        )
+    self.norm = tf.keras.layers.LayerNormalization(axis=[-2, -1])
     self.dense_temporal_projection = None
     self.dropout = tf.keras.layers.Dropout(dropout)
     self.dense_time_mixing = None
@@ -138,7 +133,6 @@ class tsmixer_res_block(tf.keras.layers.Layer):
   def get_config(self):
     config = super(tsmixer_res_block, self).get_config()
     config.update({
-      'norm_type': self.norm_type,
       'activation': self.activation,
       'dropout': self.dropout,
       'ff_dim': self.ff_dim
@@ -150,7 +144,7 @@ class tsmixer_res_block(tf.keras.layers.Layer):
     self.dense_time_mixing = tf.keras.layers.Dense(self.ff_dim, activation=self.activation)
   def call(self, inputs,*args,**kwargs):
 
-    x = self.norm(axis=[-2, -1])(inputs)
+    x = self.norm(inputs)
     x = tf.transpose(x, perm=[0, 2, 1])  # [Batch, Channel, Input Length]
     x = self.dense_temporal_projection(x)
     x = tf.transpose(x, perm=[0, 2, 1])  # [Batch, Input Length, Channel]
@@ -158,7 +152,7 @@ class tsmixer_res_block(tf.keras.layers.Layer):
     res = x + inputs
 
     # Feature Linear
-    x = self.norm(axis=[-2, -1])(res)
+    x = self.norm(res)
     x = self.dense_time_mixing(x)  # [Batch, Input Length, FF_Dim]
     x = self.dropout(x)
     x = self.dense_feature_mixing(x)  # [Batch, Input Length, Channel]
@@ -193,7 +187,7 @@ def build_model(
 if __name__ == "__main__":
   x = tf.ones((12,24,36))
   inputs =  tf.keras.layers.Input(shape=(24, 36))
-  tsmixer = tsmixer_res_block(norm_type="L", activation="gelu", dropout=0.3, ff_dim=15)
+  tsmixer = tsmixer_res_block(activation="gelu", dropout=0.3, ff_dim=15)
   print(tsmixer(x).shape)
   y = tsmixer(inputs)
 